7b80a3f16cd6d42ebf5fabfe4e1d9c42c7806cb5
ivin-austan <a.ivinaustan@gmail.com>
2025-07-29 20:39:05 +0400

    fix: updated the UI words

--- FrontEnd/src/Components/Receipts.jsx
+++ FrontEnd/src/Components/Receipts.jsx
@@ -162,7 +162,7 @@ const Receipts = () => {
                   }}
                   className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded shadow transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 cursor-pointer"
                 >
-                  Recalculate
+                  calculate
                 </button>
                 <button
                   onClick={() => {
@@ -191,7 +191,7 @@ const Receipts = () => {
               </>
             ) : (
               <>
-                <button className="px-4 py-2 invisible">Recalculate</button>
+                <button className="px-4 py-2 invisible">calculate</button>
                 <button className="px-4 py-2 invisible">
                   Reset Calculation
                 </button>
