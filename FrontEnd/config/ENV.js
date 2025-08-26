3e218b8940f3f898ac94b66d90d9b69c9ad18e94
ivin-austan <a.ivinaustan@gmail.com>
2025-08-26 17:13:55 +0400

    chore: updated the env files

--- FrontEnd/config/ENV.js
+++ FrontEnd/config/ENV.js
@@ -1,5 +1,5 @@
 export const REACT_SERVER_URL =
-  window.location.origin === "http://localhost:5173"
+  import.meta.env.VITE_ENVIRONMENT === "development"
     ? import.meta.env.VITE_REACT_SERVER_URL
-    : "https://galfar-scm-backend.onrender.com";
+    : import.meta.env.VITE_REACT_PROD_SERVER_URL;
 export const CLOUDINARY_CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
